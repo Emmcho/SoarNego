38fc6e846618149b782e7aa713bea08e70008c96
Hamid-0 <habol085@uottawa.ca>
2023-02-21 12:27:48 -0500

    added a test menu to allow users to bold

--- frontend/frontend-nego/src/SoarNego/Editor.jsx
+++ frontend/frontend-nego/src/SoarNego/Editor.jsx
@@ -2,37 +2,43 @@
 import { useContext, useEffect } from 'react';
 import FileContext from './providers/FileExporerContext';
 import { BoldExtension, CalloutExtension, ItalicExtension } from 'remirror/extensions';
-import { Remirror, useRemirror } from '@remirror/react';
+import { EditorComponent, Remirror, useRemirror } from '@remirror/react';
+import 'remirror/styles/all.css';
+
+const Menu = () => <button onClick={() => alert('TBD')}>B</button>;
 
 
-
-function Editor(){
+export const Editor=() =>{
     const {UpdateProseMirrorEditorContent} = useContext(FileContext)
-    const { manager, state } = useRemirror({
-        extensions: () => [
-          new BoldExtension(),
-          new ItalicExtension(),
-          new CalloutExtension({ defaultType: 'warn' }),
-        ],
-      
-        // Set the initial content.
-        content: '<p>I love <b>Remirror</b></p>',
-      
-        // Place the cursor at the start of the document. This can also be set to
-        // `end`, `all` or a numbered position.
-        selection: 'start',
-      
-        // Set the string handler which means the content provided will be
-        // automatically handled as html.
-        // `markdown` is also available when the `MarkdownExtension`
-        // is added to the editor.
-        stringHandler: 'html',
-      });
+      const { manager, state } = useRemirror({
+          extensions: () => [
+            new BoldExtension(),
+            new ItalicExtension(),
+            new CalloutExtension({ defaultType: 'warn' }),
+          ],
+        
+          // Set the initial content.
+          content: '<p>I love <b>Remirror</b></p>',
+        
+          // Place the cursor at the start of the document. This can also be set to
+          // `end`, `all` or a numbered position.
+          selection: 'start',
+        
+          // Set the string handler which means the content provided will be
+          // automatically handled as html.
+          // `markdown` is also available when the `MarkdownExtension`
+          // is added to the editor.
+          stringHandler: 'html',
+        });
+        
     
   return (
     <div className='remirror-theme'>
-      {/* the className is used to define css variables necessary for the editor */}
-      <Remirror manager={manager} initialContent={state} />
+      <Remirror manager={manager} initialContent={state}>
+        {/* The text editor is placed above the menu to make the zIndex easier to manage for popups */}
+        <EditorComponent />
+        <Menu />
+      </Remirror>
     </div>
   );
 
