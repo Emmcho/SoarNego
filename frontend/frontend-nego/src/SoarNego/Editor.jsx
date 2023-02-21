75bc216e34c51132bd41434223004b6fbc3b4058
Hamid-0 <habol085@uottawa.ca>
2023-02-21 12:19:26 -0500

    created framework for new extensions

--- frontend/frontend-nego/src/SoarNego/Editor.jsx
+++ frontend/frontend-nego/src/SoarNego/Editor.jsx
@@ -1,37 +1,39 @@
 
-import { WysiwygEditor } from '@remirror/react-editors/wysiwyg';
 import { useContext, useEffect } from 'react';
 import FileContext from './providers/FileExporerContext';
+import { BoldExtension, CalloutExtension, ItalicExtension } from 'remirror/extensions';
+import { Remirror, useRemirror } from '@remirror/react';
+
+
+
 function Editor(){
     const {UpdateProseMirrorEditorContent} = useContext(FileContext)
-    useEffect(() => {
-        // const mySchema = new Schema({
-        //     nodes: addListNodes(schema.spec.nodes, "paragraph block*", "block"),
-        //     marks: schema.spec.marks
-        // })
-
-        // window.view = new EditorView(document.querySelector("#editor"), {
-        //     state: EditorState.create({
-        //         doc: DOMParser.fromSchema(mySchema).parse(document.querySelector("#content")),
-        //         plugins: exampleSetup({schema: mySchema})
-                
-        //     })
-        // })
-
-        // const modView = window.view
-        // UpdateProseMirrorEditorContent(modView)
-
-        // const transaction = modView.state.tr.insert(0, modView.state.schema.text("Hello World!"))
-        // const newState = modView.state.apply(transaction);
-        // modView.updateState(newState);
-
-        
-    });
-
+    const { manager, state } = useRemirror({
+        extensions: () => [
+          new BoldExtension(),
+          new ItalicExtension(),
+          new CalloutExtension({ defaultType: 'warn' }),
+        ],
+      
+        // Set the initial content.
+        content: '<p>I love <b>Remirror</b></p>',
+      
+        // Place the cursor at the start of the document. This can also be set to
+        // `end`, `all` or a numbered position.
+        selection: 'start',
+      
+        // Set the string handler which means the content provided will be
+        // automatically handled as html.
+        // `markdown` is also available when the `MarkdownExtension`
+        // is added to the editor.
+        stringHandler: 'html',
+      });
+    
   return (
-    <div style={{ padding: 16 }}>
-                  <WysiwygEditor placeholder='Enter text...' />
-                </div>
+    <div className='remirror-theme'>
+      {/* the className is used to define css variables necessary for the editor */}
+      <Remirror manager={manager} initialContent={state} />
+    </div>
   );
 
 }
